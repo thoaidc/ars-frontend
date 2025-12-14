9bdc394224ab8f507cb4e34b3e490679fd7f3792
thoaidc <dcthoai1023@gmail.com>
2025-12-14 21:47:57 +0700

    feat(admin): add view order

--- src/app/pages/admin/order/order.component.ts
+++ src/app/pages/admin/order/order.component.ts
@@ -1,12 +1,80 @@
 import { Component } from '@angular/core';
+import {DecimalPipe, NgClass, NgForOf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {NgSelectComponent} from "@ng-select/ng-select";
+import {NgbModal, NgbModalRef, NgbPagination} from "@ng-bootstrap/ng-bootstrap";
+import {SafeHtmlPipe} from "../../../shared/pipes/safe-html.pipe";
+import {TranslatePipe, TranslateService} from "@ngx-translate/core";
+import {UtilsService} from '../../../shared/utils/utils.service';
+import {ToastrService} from 'ngx-toastr';
+import {OrderDetailComponent} from './order-detail/order-detail.component';
+import {Order, OrdersFilter, OrderViewType} from '../../../core/models/order.model';
+import {OrderService} from '../../../core/services/order.service';
+import {PAGINATION_PAGE_SIZE} from '../../../constants/common.constants';
+import {ICON_SEARCH} from '../../../shared/utils/icon';
 
 @Component({
   selector: 'app-admin-order',
   standalone: true,
-  imports: [],
+  imports: [
+    DecimalPipe,
+    FormsModule,
+    NgForOf,
+    NgSelectComponent,
+    NgbPagination,
+    SafeHtmlPipe,
+    TranslatePipe,
+    NgClass
+  ],
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss'
 })
 export class OrderComponent {
+  ordersFilter: OrdersFilter = {
+    page: 1,
+    size: 10
+  };
+  orders: Order[] = [];
+  totalItems: number = 0;
+  isLoading: boolean = false;
+  private modalRef?: NgbModalRef;
 
+  constructor(
+    private translateService: TranslateService,
+    private orderService: OrderService,
+    private utilService: UtilsService,
+    private modalService: NgbModal,
+    private toast: ToastrService
+  ) {}
+
+  ngOnInit() {
+    this.onSearch();
+  }
+
+  onSearch() {
+    this.ordersFilter.page = 1;
+    this.getOrderWithPaging();
+  }
+
+  onChangedPage(event: any): void {
+    this.ordersFilter.page = event;
+    this.getOrderWithPaging();
+  }
+
+  getOrderWithPaging() {
+    const searchRequest = this.utilService.buildFilterRequest(this.ordersFilter);
+    this.orderService.getAllWithPaging(searchRequest).subscribe(response => {
+      this.orders = response.result || [];
+      this.totalItems = response.total || this.orders.length;
+    });
+  }
+
+  view(orderId: number) {
+    this.modalRef = this.modalService.open(OrderDetailComponent, { size: 'xl', backdrop: 'static' });
+    this.modalRef.componentInstance.orderId = orderId;
+    this.modalRef.componentInstance.type = OrderViewType.ORDER;
+  }
+
+  protected readonly PAGINATION_PAGE_SIZE = PAGINATION_PAGE_SIZE;
+  protected readonly ICON_SEARCH = ICON_SEARCH;
 }
--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,14 +1,20 @@
 import {Component, OnInit} from '@angular/core';
 import {DecimalPipe, NgClass, NgForOf} from "@angular/common";
 import {NgSelectComponent} from "@ng-select/ng-select";
-import {NgbPagination, NgbTooltip} from "@ng-bootstrap/ng-bootstrap";
+import {NgbModal, NgbModalRef, NgbPagination} from "@ng-bootstrap/ng-bootstrap";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SafeHtmlPipe} from "../../../shared/pipes/safe-html.pipe";
-import {TranslatePipe} from "@ngx-translate/core";
+import {TranslatePipe, TranslateService} from "@ngx-translate/core";
 import {PAGINATION_PAGE_SIZE} from '../../../constants/common.constants';
 import {ICON_COPY, ICON_DELETE, ICON_PLUS, ICON_SEARCH, ICON_UPDATE} from '../../../shared/utils/icon';
 import {Authorities} from '../../../constants/authorities.constants';
 import {Product, ProductsFilter} from '../../../core/models/product.model';
+import {ProductService} from '../../../core/services/product.service';
+import {UtilsService} from '../../../shared/utils/utils.service';
+import {ToastrService} from 'ngx-toastr';
+import {
+  ModalUpdateProductComponent
+} from '../../shop/products/product-design/modal-update-product/modal-update-product.component';
 
 @Component({
   selector: 'app-admin-products',
@@ -22,8 +28,7 @@ import {Product, ProductsFilter} from '../../../core/models/product.model';
     SafeHtmlPipe,
     TranslatePipe,
     FormsModule,
-    NgClass,
-    NgbTooltip
+    NgClass
   ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
@@ -37,6 +42,15 @@ export class ProductsComponent implements OnInit {
   products: Product[] = [];
   totalItems: number = 0;
   isLoading: boolean = false;
+  private modalRef?: NgbModalRef;
+
+  constructor(
+    private translateService: TranslateService,
+    private productService: ProductService,
+    private utilService: UtilsService,
+    private modalService: NgbModal,
+    private toast: ToastrService
+  ) {}
 
   ngOnInit() {
     this.onSearch();
@@ -53,19 +67,17 @@ export class ProductsComponent implements OnInit {
   }
 
   getProductWithPaging() {
-
+    const searchRequest = this.utilService.buildFilterRequest(this.productsFilter);
+    this.productService.getAllWithPaging(searchRequest).subscribe(response => {
+      this.products = response.result || [];
+      this.totalItems = response.total || this.products.length;
+    });
   }
 
   view(productId: number) {
-
-  }
-
-  openModalCreateProduct(productId?: number) {
-
-  }
-
-  delete(product: Product) {
-
+    this.modalRef = this.modalService.open(ModalUpdateProductComponent, { size: 'xl', backdrop: 'static' });
+    this.modalRef.componentInstance.productId = productId;
+    this.modalRef.componentInstance.isUpdatable = false;
   }
 
   protected readonly PAGINATION_PAGE_SIZE = PAGINATION_PAGE_SIZE;
