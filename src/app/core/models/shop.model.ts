8de48e10ba533e3b4a83ae355d310b661adcd31d
thoaidc <dcthoai1023@gmail.com>
2025-12-14 22:11:20 +0700

    feat(admin): add view shops

---
+++ src/app/core/models/shop.model.ts
@@ -0,0 +1,15 @@
+import {BaseFilterRequest} from './request.model';
+import {AuditingEntity} from './common.model';
+
+export interface ShopsFilter extends BaseFilterRequest {
+
+}
+
+export interface Shop extends AuditingEntity {
+  name: string;
+  ownerId: number;
+  ownerName: string;
+  ownerEmail: string;
+  ownerPhone: string;
+  status: string;
+}
