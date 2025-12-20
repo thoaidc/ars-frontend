26f70caa6d6fbb76e7cb2dbeae063e14a7748c16
thoaidc <dcthoai1023@gmail.com>
2025-12-20 13:37:12 +0700

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  // Use empty SERVER_API_URL during dev so requests go to relative /api and can be proxied by Angular dev server
-  SERVER_API_URL: 'https://3aa822937d98.ngrok-free.app'
+  SERVER_API_URL: ''
 };
